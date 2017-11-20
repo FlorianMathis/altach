be1e0789824a7b879347f3d669eb1123b4da2e7d
Florian Mathis <mathis@cip.ifi.lmu.de>
2017-11-21 00:03:04 +0100

    initial commit

---
+++ js/controllers.js
@@ -0,0 +1,58 @@
+angular.module('app.controllers', [])
+  
+.controller('aSBCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('tickerCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('tabelleCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('twitterCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('sCRAATCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('newsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ js/routes.js
@@ -0,0 +1,81 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('tabsController.aSB', {
+    url: '/page3',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/aSB.html',
+        controller: 'aSBCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.ticker', {
+    url: '/page4',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/ticker.html',
+        controller: 'tickerCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.tabelle', {
+    url: '/page5',
+    views: {
+      'tab5': {
+        templateUrl: 'templates/tabelle.html',
+        controller: 'tabelleCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.twitter', {
+    url: '/page6',
+    views: {
+      'tab6': {
+        templateUrl: 'templates/twitter.html',
+        controller: 'twitterCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.sCRAAT', {
+    url: '/page7',
+    views: {
+      'tab7': {
+        templateUrl: 'templates/sCRAAT.html',
+        controller: 'sCRAATCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.news', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/news.html',
+        controller: 'newsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+$urlRouterProvider.otherwise('/page1/page2')
+
+
+});
